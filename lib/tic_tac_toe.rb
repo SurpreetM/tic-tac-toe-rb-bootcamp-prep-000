07d834fe16f003bae4aad4bcc980d81f9897a521
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-26 14:10:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,61 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index (user_input)
+  user_input.to_i - 1 
+end 
+
+def move (board, index, player)
+  board[index] = player
+end 
+
+def position_taken?(board, index)
+  board[index]== "X" || board[index] == "O"
+end 
+
+def valid_move? (board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end   
+
+def turn (board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index (user_input)
+  if valid_move?(board,index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else 
+    puts "That move is invalid"
+    turn (board)
+  end 
+end 
+
+def turn_count(board)
+  turns = 0 
+    board.each do |token| 
+    if token == "X" || token == "O"
+      turn += 1
+    end 
+  end 
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end 
 
