b445053802213508aa4124bb8b487d63ba25f042
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-26 14:31:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,5 +94,12 @@ def winner(board)
 end 
 
 def play(board)
-  input = gets
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
 end 
